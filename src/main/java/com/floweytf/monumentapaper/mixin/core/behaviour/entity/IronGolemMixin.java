7447229510e2095aaef9c2a8fbd474a4a2f45c05
Floweynt <guywithanaxe42@gmail.com>
2024-06-16 01:10:49 -0500

    feat: remove minecraft stupidity (symlink validation)

--- src/main/java/com/floweytf/monumentapaper/mixin/core/behaviour/entity/IronGolemMixin.java
+++ src/main/java/com/floweytf/monumentapaper/mixin/core/behaviour/entity/IronGolemMixin.java
@@ -3,7 +3,6 @@ package com.floweytf.monumentapaper.mixin.core.behaviour.entity;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.animal.IronGolem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
